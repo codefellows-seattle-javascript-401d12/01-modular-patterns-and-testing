59ef547d33026792737aed9b2b9f5d5e0f5984c8
Hawa Abdi <abdih17@hotmail.com>
2016-12-06 10:16:54 -0800

    Four passing test.

--- tests/test.js
+++ tests/test.js
@@ -6,13 +6,24 @@ var name = process.argv[2];
 
 describe('Greet Module', function() {
   describe('#sayHi', function() {
-    it('should return hi {name}!', function() {
+    it('should return Hi {name}!', function() {
       var result = greet.sayHi(name);
-      assert.ok(result === `hi ${name}!`, 'not equal to hi hawa!');
+      assert.ok(result === `Hi ${name}!`, 'not equal to Hi hawa!');
     });
     it('should throw a missing name error', function() {
       assert.throws(function() {
         greet.sayHi();
+      }, 'error not thrown');
+    });
+  });
+  describe('#saySeeYaLater', function() {
+    it('should return See ya later {petName}!', function(){
+      var results = greet.saySeeYaLater('Gaara');
+      assert.ok(results === 'See ya later, Gaara!', 'not equal to See ya later, Gaara!');
+    });
+    it('should throw a missing name error', function(){
+      assert.throws(function(){
+        greet.saySeeYaLater();
       }, 'error not thrown');
     });
   });
