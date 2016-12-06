c488e082339bda95c7c186398ebcfb74787ccde1
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-12-05 16:44:29 -0800

    Set up greet-test set up tests on greet-test.js file and ran test in terminal. All tests currently passing!

--- test/greet-test.js
+++ test/greet-test.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const greet = require('../lib/greet.js');
+const assert = require('assert');
+
+describe('Test greet file', function() {
+  describe('#sayHey', function() {
+    it('should return what\'s up {name}', function() {
+      assert.ok(greet.sayHey('jonny') === 'what\'s up jonny!', 'sayHey function does not return what\'s up jonny');
+    });
+    it('should return missing name error', function() {
+      assert.throws(function() {
+        greet.sayHey();
+      }, 'error not thrown when it should have been');
+    });
+  });
+  describe('#sayBye', function() {
+    it('should return see ya later {name}', function() {
+      assert.ok(greet.sayBye('jonny') === 'See ya later jonny!', 'sayBye function does not return see ya later jonny!');
+    });
+    it('should return missing name error', function(){
+      assert.throws(function() {
+        greet.sayBye();
+      }, 'error not thrown when it should have been');
+    });
+  });
+});
