0b25e84d494f95b8d320ea80024da187b1f39cc1
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-06 13:23:17 -0800

    add nested describe statement for welcome function

--- test/greeting-test.js
+++ test/greeting-test.js
@@ -5,13 +5,15 @@ const greeting = require('../lib/greeting.js');
 const assert = require('assert');
 
 describe('Greeting', function() {
-  it('should print a greeting message customized with the user\'s inputted name.', function() {
-    var result = greeting.welcome('Caleb');
-    assert.ok(result === 'Hello and welcome, Caleb! Glad you can be here!', 'greeting not printed as expected');
-  });
-  it('should throw an error indicating a missing name or unexpected charater', function() {
-    assert.throws(function() {
-      greeting.welcome();
-    }, 'error not thrown');
+  describe('#welcome', function() {
+    it('should print a greeting message customized with the user\'s inputted name.', function() {
+      var result = greeting.welcome('Caleb');
+      assert.ok(result === 'Hello and welcome, Caleb! Glad you can be here!', 'greeting not printed as expected');
+    });
+    it('should throw an error indicating a missing name or unexpected charater', function() {
+      assert.throws(function() {
+        greeting.welcome();
+      }, 'error not thrown');
+    });
   });
 });
