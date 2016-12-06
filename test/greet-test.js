29d9eaa4cc4790aea1b6d8ce1370216f5ef11fc8
kcirekcom <erick.f.mock@gmail.com>
2016-12-05 20:02:46 -0800

    updated comment

--- test/greet-test.js
+++ test/greet-test.js
@@ -5,10 +5,10 @@ const assert = require('assert');
 
 describe('Greet Test Module', function () {
   describe('#greet', function () {
-    //pointing back to run sayHello function
+    //pointing back to run greet function
     it(`should return hello ${process.argv[2]}!`, function () {
       var result = greet.greet(process.argv[2]);
-      //creating variable that stores value of sayHello
+      //creating variable that stores value of greet
       assert.ok(result === `hello ${process.argv[2]}!`, `not equal to hello ${result}!`);
       //using ok method to display result status
     });
