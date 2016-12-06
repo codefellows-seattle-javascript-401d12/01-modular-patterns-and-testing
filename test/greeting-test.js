8297227e51743a9a488aded8a1ed92747b4e82bc
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-06 11:36:43 -0800

    build out basic tests

---
+++ test/greeting-test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const greeting = require('../lib/greeting.js');
+
+const assert = require('assert');
+
+describe('Greeting', function() {
+  it('should print a greeting message customized with the user\'s inputted name.', function() {
+    var result = greeting.welcome('Caleb');
+    assert.ok(result === 'Hello and welcome, Caleb! Glad you can be here!', 'greeting not printed as expected');
+  });
+  it('should throw an error indicating a missing name or unexpected charater', function() {
+    assert.throws(function() {
+      greeting.welcome();
+    }, 'error not thrown');
+  });
+});
