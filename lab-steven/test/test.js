83e3788e7bce0a1da399ca23ab923786c86f48cc
Steven Bateman <batemanvo@gmail.com>
2016-12-05 16:08:57 -0800

    Added a test for whether the name is undefined.

--- lab-steven/test/test.js
+++ lab-steven/test/test.js
@@ -9,5 +9,9 @@ describe('Greet', function() {
       var result = greet.greet(process.argv[2]);
       assert.ok(result === `hello ${process.argv[2]}`, `Instead says ${result}`);
     });
+
+    it('Name expected to be undefined when using mocha', function() {
+      assert.ok(greet.greet() === 'hello undefined', 'Did not come back as hello undefined');
+    });
   });
 });
