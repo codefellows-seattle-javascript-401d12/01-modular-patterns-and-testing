0c71bcae9b20317671f21c4ef994a39341f42950
kcirekcom <erick.f.mock@gmail.com>
2016-12-05 17:56:08 -0800

    added .eslintrc file, completed bonus, rearranged files

--- test/greet-test.js
+++ test/greet-test.js
@@ -3,19 +3,19 @@
 const greet = require('../lib/greet.js');
 const assert = require('assert');
 
-describe('Greet Module', function () {
+describe('Greet Test Module', function () {
   describe('#greet', function () {
     //pointing back to run sayHello function
     it('should return hello erick!', function () {
-      var result = greet.greet('erick');
+      var result = greet.greet(process.argv[2]);
       //creating variable that stores value of sayHello
-      assert.ok(result === 'hello erick!', 'not equal to hello erick!');
+      assert.ok(result === `hello ${process.argv[2]}!`, `not equal to hello ${result}!`);
       //using ok method to display result status
     });
     it('should throw error that name is missing', function () {
       assert.throws(function() {
         greet.greet();
-      }, 'error not thrown');
+      }, 'no error thrown');
     });
   });
 });
